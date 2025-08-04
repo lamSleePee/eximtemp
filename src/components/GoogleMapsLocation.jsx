935df19928da22258a7b953151ddcb9284ab7760
IamSleePee <lalit1may@gmail.com>
2025-08-04 20:00:30 +0530

    address added

--- src/components/GoogleMapsLocation.jsx
+++ src/components/GoogleMapsLocation.jsx
@@ -8,6 +8,7 @@ function GooglemapsLocation(){
                     <h2>Visit Us</h2>
                     <p>We are located in the heart of the city, easily accessible by public transport.</p>
                     <p className="address"><i class="fa-solid fa-house"></i>  <a className='gmap-icon' href="https://www.google.com/maps/place/Exim+Pinnacle+Enterprises/@18.5752515,73.7662661,17z/data=!3m1!4b1!4m6!3m5!1s0xaaeb5a9d0808ea71:0xc440ce0f3d7f1604!8m2!3d18.5752515!4d73.7662661!16s%2Fg%2F11xl9mq5s6?entry=ttu&g_ep=EgoyMDI1MDcxNi4wIKXMDSoASAFQAw%3D%3D">Exim Pinnacle Enterprises, TBL Space, Balewadi Plaza, Balewadi, Pune, Maharashtra 411025</a></p>
+                    <p className="address"><i class="fa-solid fa-house"></i>  <a className='gmap-icon' href="https://www.google.com/maps/search/37-Satakunnankatu,Tampere,Finland-33210">37- Satakunnankatu, Tampere, Finland-33210</a></p>
                     <p className="email"><i class="fa-solid fa-envelope"></i> <a className='gmap-icon' href="mailto:Info@eximpinnacle.com">Info@eximpinnacle.com</a></p>
                     <p className="phoneNo"><i class="fa-solid fa-phone"></i>  <a className='gmap-icon' href="tel:9607799399">9607799399</a> / <a className='gmap-icon' href="tel:7350599399">7350599399</a></p> 
                 </div>
